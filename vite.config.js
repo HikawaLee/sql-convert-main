3678a9b96e905bf042b51951a51541be423264e4
HikawaLee <zhao-meng-yu@ncuhome.club>
2024-03-20 18:53:15 +0800

    :sparkles: 打包优化

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,21 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import { fileURLToPath, URL } from 'node:url'
-
+import { visualizer } from 'rollup-plugin-visualizer'
+import { viteSingleFile } from 'vite-plugin-singlefile';
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(),
+  base: './',
+  plugins: [
+
+
+    // 打包体积分析
+    visualizer({
+      open: true,
+      filename: 'visualizer.html' //分析图生成的文件名
+    }),
+    viteSingleFile(),
+    vue(),
     AutoImport({
       imports:['vue'],
       dts:'src/auto-import.d.ts'
@@ -14,6 +25,22 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 1500,
+    minify: false,
+    rollupOptions: {
+      output: {
+        chunkFileNames: 'js/[name]-[hash].js', // 引入文件名的名称
+        entryFileNames: 'js/[name]-[hash].js', // 包的入口文件名称
+        assetFileNames: '[ext]/[name]-[hash].[ext]', // 资源文件像 字体，图片等
+      },
+      manualChunks(id) {
+        if (id.includes('node_modules')) {
+          return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        }
+      }
     }
   }
 })
