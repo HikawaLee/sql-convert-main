634aa10ee1f44cf6d60db33fbbf96f8e8bebe50c
HikawaLee <zhao-meng-yu@ncuhome.club>
2024-03-25 17:04:06 +0800

    add conf

---
+++ src/types/tableConfKeys.js
@@ -0,0 +1,10 @@
+export default {
+    DESC: 'desc',
+    LAYOUT: 'layout',
+    COMPONENT_TYPE: 'componentType',
+    TITLE: 'title',
+    REQUIRED: 'required',
+    REQUIRED_MSG: 'requiredMsg',
+    VALUE: 'value',
+
+}
